56150a66b0fc83139af51daf0ba33a385a290d68
s3728807 <vdvillaflores@gmail.com>
2020-09-18 15:48:10 +1000

    initial linkedlist header

---
+++ headers/LinkedList.h
@@ -0,0 +1,16 @@
+#ifndef LINKEDLIST
+#define LINKEDLIST
+
+#include <iostream>
+#include "Player.h"
+
+namespace Azul
+{
+    class LinkedList
+    {
+        public:
+            Player *p;
+    };
+};
+
+#endif
